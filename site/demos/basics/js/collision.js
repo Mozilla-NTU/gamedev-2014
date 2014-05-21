a80f1c4618268e16f12799fa041d3b6063e8c572
Michael Henretty <michael.henretty@gmail.com>
2014-05-21 05:40:53 -0700

    updating slides from live editing

--- site/demos/basics/js/collision.js
+++ site/demos/basics/js/collision.js
@@ -6,11 +6,36 @@ Collider.prototype.addBall = function(ball) {
   this.balls.push(ball);
 }
 
+Collider.prototype.checkCollision = function() {
+  for (var i = 0; i < this.balls.length; i++) {
+    for (var j = i + 1; j < this.balls.length; j++) {
+      var b1 = this.balls[i];
+      var b1Center = b1.getCenter();
+      var b2 = this.balls[j];
+      var b2Center = b2.getCenter();
+      var distance = Math.sqrt(Math.pow(b2Center.x - b1Center.x, 2) + Math.pow(b2Center.y - b1Center.y, 2));
+      if (distance < b1.radius + b2.radius) {
+        b1.vy = -b1.vy;
+        b2.vy = -b2.vy;
+        b1.vx = -b1.vx;
+        b2.vx = -b2.vx;
+      }
+    }
+  }
+}
+
 function Ball(stage) {
   this.stage = stage;
   this.el = document.createElement('div');
   this.el.classList.add('ball');
   this.stage.appendChild(this.el);
+
+  this.diameter = (20 + (Math.random() * 80)) | 0;
+  this.radius = this.diameter / 2;
+  console.log(this.radius);
+
+  this.el.style.width = this.diameter + 'px';
+  this.el.style.height = this.diameter + 'px';
 
   this.x = (Math.random() * window.innerWidth) | 0;
   this.y = (Math.random() * window.innerHeight) | 0;
@@ -18,6 +43,13 @@ function Ball(stage) {
   this.vx = -200 + (Math.random() * 400) | 0;
   this.vy = -200 + (Math.random() * 400) | 0;
 }
+
+Ball.prototype.getCenter = function () {
+  return {
+    x: this.x + this.radius,
+    y: this.y + this.radius
+  };
+};
 
 Ball.prototype.update = function (delta) {
   this.x += (this.vx * delta) / 1000;
@@ -42,6 +74,7 @@ Ball.prototype.update = function (delta) {
 
 function Game(stage) {
   this.stage = stage;
+  this.collider = new Collider();
 
   this.balls = [];
   document.body.addEventListener('click', this.addBall.bind(this));
@@ -51,7 +84,9 @@ function Game(stage) {
 }
 
 Game.prototype.addBall = function() {
-  this.balls.push(new Ball(this.stage));
+  var ball = new Ball(this.stage);
+  this.balls.push(ball);
+  this.collider.addBall(ball);
 }
 
 Game.prototype.tick = function() {
@@ -61,6 +96,7 @@ Game.prototype.tick = function() {
   for (var i = 0; i < this.balls.length; i++) {
     this.balls[i].update(delta);
   }
+  this.collider.checkCollision();
   requestAnimationFrame(this.tick.bind(this));
 }
 
